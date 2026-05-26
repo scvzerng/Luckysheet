974b17d812e2e05149e0a04698b9315756dcc9df
scvzerng <scvzerng@163.com>
2026-05-26 12:37:47 +0800

    ﻿refactor: 移除 expendPlugins 扩展插件和 pivotTable 数据透视表
    - 删除 chart 扩展插件（chartmix.css/umd/js）
    - 删除 exportXlsx 导出插件
    - 删除 print 打印插件（plugin.js/print.css/print.js）
    - 删除 pivotTable.js 控制器
    - 删除 sheetPivotTable.js 和 sheetPivotTableData.js 示例数据
    - 清理 expendPlugins.js 中相关引用

--- src/controllers/expendPlugins.js
+++ src/controllers/expendPlugins.js
@@ -1,30 +1,9 @@
-import { chart } from '../expendPlugins/chart/plugin'
-import { print } from '../expendPlugins/print/plugin'
-import { exportXlsx } from '../expendPlugins/exportXlsx/plugin'
-
-const pluginsObj = {
-    'chart':chart,
-    'print':print,
-    'exportXlsx':exportXlsx
-}
-
-const isDemo = true
-
 /**
  * Register plugins
- * 
- * plugins:[
- * {name:'chart'},
- * {name:'print'},
- * {name:'exportXlsx',config:{url:''}}
- * ]
+ *
+ * plugins:[]
  */
 function initPlugins(plugins , options){
-    if(plugins.length){
-        plugins.forEach(plugin => {
-            pluginsObj[plugin.name](options, plugin.config, isDemo)
-        });
-    }
 }
 
 export {
--- src/expendPlugins/print/print.js
+++
@@ -1,3 +0,0 @@
-const luckysheetPrint = null;
-
-export { luckysheetPrint };
