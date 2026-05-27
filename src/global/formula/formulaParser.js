c8931933caeab8ce020744483151a210da6f9132
scvzerng <scvzerng@163.com>
2026-05-27 14:23:53 +0800

    ﻿fix: 修复 formulaParser 中 ls 变量未声明导致 ReferenceError
    - for 循环变量 ls 缺少 let 声明，在严格模式下报 ReferenceError: ls is not defined

--- src/global/formula/formulaParser.js
+++ src/global/formula/formulaParser.js
@@ -691,7 +691,7 @@ const formulaParser = {
                             }
 
                             // 修复类似=1--1、=1---1、=1+--+1--1这类连续+-混合写法计算结果和wps和office不一致的bug, by @kdevilpf 2023-10-08
-                            for (ls = i + 1; ls < funcstack.length; ls++) {
+                            for (let ls = i + 1; ls < funcstack.length; ls++) {
                                 if (["--", "++"].includes(s + funcstack[ls])) {
                                     s = "+";
                                 } else if (["-+", "+-"].includes(s + funcstack[ls])) {
