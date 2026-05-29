b9c76fb3aa148fd161217a88b5edea0d4bc76717
scvzerng <scvzerng@163.com>
2026-05-30 00:31:13 +0800

    fix: 修复sheetInit.js null安全和format.js空格式防御
    - sheetInit.js: Store.luckysheet_select_save[0].row添加null检查
    - sheetInit.js: data[0].length添加空数组防御
    - format.js: write_num_flt/write_num_int添加空fmt防御
    - 修复TypeError: Cannot read properties of undefined (reading 'row')

--- src/controllers/sheetmanage/sheetInit.js
+++ src/controllers/sheetmanage/sheetInit.js
@@ -31,15 +31,18 @@ const sheetInitModule = {
     }));
     file.luckysheet_select_save = select_save;
     this.sheetParamRestore(file, data);
-    let r2 = Store.luckysheet_select_save[0].row[1],
+    let r2 = 0, c2 = 0;
+    if (Store.luckysheet_select_save && Store.luckysheet_select_save.length > 0 && Store.luckysheet_select_save[0].row) {
+      r2 = Store.luckysheet_select_save[0].row[1];
       c2 = Store.luckysheet_select_save[0].column[1];
-    if (Store.luckysheet_select_save.length > 1) {
-      for (let i = 0; i < Store.luckysheet_select_save.length; i++) {
-        if (Store.luckysheet_select_save[i].row[1] > r2) {
-          r2 = Store.luckysheet_select_save[i].row[1];
-        }
-        if (Store.luckysheet_select_save[i].column[1] > c2) {
-          c2 = Store.luckysheet_select_save[i].column[1];
+      if (Store.luckysheet_select_save.length > 1) {
+        for (let i = 0; i < Store.luckysheet_select_save.length; i++) {
+          if (Store.luckysheet_select_save[i].row && Store.luckysheet_select_save[i].row[1] > r2) {
+            r2 = Store.luckysheet_select_save[i].row[1];
+          }
+          if (Store.luckysheet_select_save[i].column && Store.luckysheet_select_save[i].column[1] > c2) {
+            c2 = Store.luckysheet_select_save[i].column[1];
+          }
         }
       }
     }
@@ -50,7 +53,7 @@ const sheetInitModule = {
     if (r2 > rowheight - 1) {
       rowheight = r2 + 1;
     }
-    let colwidth = data[0].length;
+    let colwidth = data.length > 0 && data[0] ? data[0].length : 0;
     if (c2 > colwidth - 1) {
       colwidth = c2 + 1;
     }
